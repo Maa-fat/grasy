dea03b46daa173b3b07e5cb42e15aade24f9c170
Maa-fat <sychmydog5@icloud.com>
2026-08-06 23:04:56 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "app-v3";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key); // 古い削除
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        // ✅ 最新をキャッシュに保存
+        let resClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, resClone);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        // ✅ オフライン時だけキャッシュ使う
+        return caches.match(event.request);
+      })
+  );
+});
+``
